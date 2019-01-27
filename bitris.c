46e82a72110ff7ca8714f3d2536ae390d4eea255
bnaucler <mail@bnaucler.se>
2019-01-27 22:40:22 +0100

    playable version

--- bitris.c
+++ bitris.c
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include <ncurses.h>        // test impl
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
 
 #define PFH 20
@@ -20,9 +19,6 @@
 
 #define DEBUG 1
 
-#define DEL 0
-#define PLOT 1
-
 #define ROWMX 1023
 
 /* Reference material
@@ -32,9 +28,8 @@
  *
  */
 
-
 typedef struct {
-    int8_t block, pos, x, y;
+    int8_t block, pos, x, y; // OR block & pos together?
 } piece;
 
 const uint16_t blk[NUMPCS][4] = {
@@ -48,7 +43,7 @@ const uint16_t blk[NUMPCS][4] = {
     {3840, 17476, 3840, 17476},   // I
 };
 
-const uint16_t scoremp[5] = {0, 40, 100, 300, 1200}; // score multipliers
+const uint16_t smp[5] = {0, 40, 100, 300, 1200}; // score multipliers
 
 /* TODO: Implement algo to calculate the following drop speeds
    NES configuration:
@@ -72,6 +67,10 @@ const uint16_t scoremp[5] = {0, 40, 100, 300, 1200}; // score multipliers
     29+             1 (0.02s)
 
     or this memory monster, in the worst case
+    or a uint8_t * 10..?
+
+800 720 630 550 470 380 300 220 130 100 80 80 80 70 70 70 50 50 50 30 30 30 30 30 30 30 30 30 30 20
+
     const uint16_t speed[] = 800, 720, 630, 550, 470, 380, 300, 220, 130, 100,
                              80, 80, 80, 70, 70, 70, 50, 50, 50, 30, 30, 30,
                              30, 30, 30, 30, 30, 30, 30, 20;
@@ -80,19 +79,13 @@ const uint16_t scoremp[5] = {0, 40, 100, 300, 1200}; // score multipliers
 static void newpiece(piece *p) {
 
     // NES style piece selection algo
-    uint8_t npnum = rand() % NUMPCS + 1;
-    if(npnum == NUMPCS || npnum == p->block) p->block = rand() % NUMPCS;
-    else p->block = npnum;
+    uint8_t np = rand() % NUMPCS + 1;
+    if(np == NUMPCS || np == p->block) p->block = rand() % NUMPCS;
+    else p->block = np;
 
     p->pos = 0;
     p->x = XSTART;
     p->y = YSTART;
-}
-
-// Applies left or right shift
-static uint16_t mkshift(uint16_t val, int8_t shift) {
-
-    return shift < 0 ? val << abs(shift) : val >> shift;
 }
 
 // clears full lines - returns score
@@ -109,150 +102,152 @@ static uint16_t cline(uint16_t *pf, uint16_t *lines, uint8_t *lev, uint8_t py) {
     }
 
     *lines += newlines;
+    *lev = *lines / 10;
 
-    return scoremp[newlines] * (*lev + 1);
+    return smp[newlines] * (*lev + 1);
 }
 
-// set values to bitshift row -- TODO: can this be simplified? double shift
-static void setrow(piece *p, uint8_t row, uint16_t *rval) {
+// gets real x & y coordinates for block pieces
+static int8_t setxy(const piece *p, const int8_t i, int8_t *x, int8_t *y) {
 
-    row -= p->y;
-    *rval = blk[p->block][p->pos] & 15 << row * 4;
-    *rval = mkshift(*rval, row * 4 - p->x);
-}
-
-// receives row value and shift - returns 1 for collision TODO: clean up
-static uint8_t iscol(uint16_t *pf, uint16_t rval, uint16_t prval, int8_t y, int8_t rowx) {
-
-    if(!rval || !pf[y + 1]) return 0;
-
-    uint16_t pbx, pbrx;
-
-    for(uint8_t x = rowx; x < rowx + 4; x++) {
-        pbx = rval & 1 << x; // bit value for current line of piece
-        pbrx = prval & 1 << x; // bit value for previous line of piece
-        if(pbx != pbrx && pbx && pbx == (pf[y + 1] & 1 << x)) return 1;
-    }
-
+    if(!(blk[p->block][p->pos] & 1 << i)) return 1;
+    *y = i / 4 + p->y;
+    *x = i % 4 + p->x;
+    
     return 0;
 }
 
-// checks for collisions -- TODO: seems glitchy
-static uint8_t ccol(uint16_t *pf, piece *p) {
+// checks for collisions
+static uint8_t ccol(const uint16_t *pf, const piece *p) {
 
-    // TODO: actual collision; piece should have 1 cycle to move
-    //       modify to enable tuck / spin checks
+    int8_t x, y, i = 0;
 
-    uint16_t rval, prval;
-
-    for(int8_t y = p->y + 3; y >= p->y; y--) {
-        prval = rval;
-        setrow(p, y, &rval);
-
-        if(y == PFH - 1 && rval) return 1; // screen bottom
-        else if(iscol(pf, rval, prval, y, p->x)) return 1;
-    }
+    do {
+        if(setxy(p, i, &x, &y)) continue;
+        if(x < 0 || x > PFW - 1 || y > PFH - 1 || pf[y] & 1 << x) return 1;
+    } while(++i < 16);
 
     return 0;
 }
 
 // updates pf by applying act to rows of p
-static void mvpiece(uint16_t *pf, piece *p, uint8_t act) {
+static void plotpiece(uint16_t *pf, const piece *p, const uint8_t val) {
 
-    uint16_t rval;
+    int8_t x, y, i = 0;
 
-    for(int8_t y = p->y; y < p->y + 4; y++) {
-        setrow(p, y, &rval);
+    do {
+        if(setxy(p, i, &x, &y)) continue;
 
-        if(act == DEL) pf[y] = pf[y] & ~rval;
-        else pf[y] = pf[y] | rval; // assume PLOT
-    }
+        if(val) pf[y] |= 1 << x;
+        else pf[y] &=  ~(1 << x);
+    } while(++i < 16);
 }
 
 // draws the playing field - curses version
-static void draw(const uint16_t *pf, piece *p, uint16_t lines, uint16_t score) {
+static void draw(const uint16_t *pf, piece *p, const uint8_t lev, const uint16_t lines, const uint16_t score) {
 
     erase();
 
-    for(int y = 0; y < PFH; y++) {
-        for(int x = 0; x < PFW; x++) {
-            if(pf[y] & 1 << x) addch(ACS_CKBOARD);
-            else addch('.');
-        }
-
+    for (uint8_t y = 0; y < PFH; y++){
+        for(uint8_t x = 0; x < PFW; x++)
+            addch(pf[y] & 1 << x ? ACS_CKBOARD : '.');
         if(DEBUG) printw("   pf[%d] = %d\n", y, pf[y]);
         else addch('\n');
-    }
+    } 
 
-    printw("lines: %d\tscore: %d\n", lines, score);
+    printw("level: %d\tlines: %d\tscore: %d\n", lev, lines, score);
     if(DEBUG) printw("DEBUG: p->block = %d[%d], bval = %d, p->x = %d, p->y = %d\n",
             p->block, p->pos, blk[p->block][0], p->x, p->y);
 
     refresh();
 }
 
+// raw memory copy of piece s to d
+static void cppiece(piece *d, const piece *s) {
+
+    uint8_t sz = sizeof(d);
+    char *pd = (char*) d;
+    const char *ps = (char*) s;
+
+    while(--sz) *pd++ = *ps++;
+}
+
+
+// Applies transformation to piece based on user input - curses impl
+static uint8_t getinput(piece *p) {
+
+    char c = getch();
+    switch(c) {
+
+        case 'q':
+            return 1;
+            break;
+
+        case 'a':
+            p->x--;
+            break;
+
+        case 's':
+            p->x++;
+            break;
+
+        case 'k':
+            p->pos++;
+            if(p->pos > 3) p->pos = 0; 
+            break;
+
+        case 'j':
+            p->pos--;
+            if(p->pos < 0) p->pos = 3;
+            break;
+    }
+
+    return 0;
+}
+
 int main(void) {
 
-	time_t t;
-	srand((unsigned) time(&t));
+	srand(time(NULL));
+    rand();
 
     uint16_t pf[PFH];
 
     uint16_t score = 0, lines = 0;
     uint8_t lev = 0;
 
-    for(int a = 0; a < PFH; a++) pf[a] = 0;
+    for(uint8_t i = 0; i < PFH; i++) pf[i] = 0;
 
     // curses impl
     initscr();
     raw();
     noecho();
+    nodelay(stdscr, 1);
     timeout(200);
 
-    piece p;
+    piece p, n;
     newpiece(&p);
 
-    // TODO: plot first in loop
     for(;;) {
+    // while(!go) {
+        plotpiece(pf, &p, 1);
+        draw(pf, &p, lev, lines, score);
+        plotpiece(pf, &p, 0);
 
-        if(ccol(pf, &p)) {
+        cppiece(&n, &p);
+        n.y++; // drop
+
+        if(ccol(pf, &n)) {
+            plotpiece(pf, &p, 1);
             score += cline(pf, &lines, &lev, p.y);
             newpiece(&p);
+            if(ccol(pf, &p)) break;
 
         } else {
-            mvpiece(pf, &p, DEL);
+            cppiece(&p, &n);
         }
 
-        char c = getch();
-        switch(c) {
-
-            case 'q':
-                return 0;
-
-            case 'a':
-                p.x--;
-                if(p.x < -1) p.x = -1; // TODO: Check for piece boundry
-                break;
-
-            case 's':
-                p.x++;
-                if(p.x > PFW - 3) p.x = PFW - 3; // TODO: Check for piece boundry
-                break;
-
-            case 'j':
-                p.pos++;
-                if(p.pos > 3) p.pos = 0; // TODO: check for collision, enable tuck & spin
-                break;
-
-            case 'k':
-                p.pos--;
-                if(p.pos < 0) p.pos = 3; // TODO: check for collision, enable tuck & spin
-                break;
-        }
-
-        p.y++; // drop
-        mvpiece(pf, &p, PLOT);
-        draw(pf, &p, lines, score); // p for debug
+        if(getinput(&n)) break;
+        if(!ccol(pf, &n)) cppiece(&p, &n);
     }
 
     endwin(); // curses impl
